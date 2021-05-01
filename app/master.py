34cc147aa48add13e8ab27259de512cf89aa9f6c
Sergio Chouhy <sergio.chouhy@gmail.com>
2021-05-01 22:20:20 +0000

    fix bug

--- app/master.py
+++ app/master.py
@@ -7,6 +7,8 @@ from datetime import datetime
 
 from exp.dataset import SimpleAlphaZeroDataset
 
+import click
+
 import logging
 
 logging.basicConfig(filename='/app/log',
@@ -50,18 +52,16 @@ class Master:
         logging.log('this should trigger weight updates')
 
 
-@app.route('/turn_on')
+@app.cli.command('turn_on')
 def turn_on():
-    logging.info('Hit turn_on')
+    logging.info('Command turn_on')
     master.turn_on()
-    return 'OK'
 
 
-@app.route('/turn_off')
+@app.cli.command('turn_off')
 def turn_off():
-    logging.info('Hit turn_off')
+    logging.info('Command turn_off')
     master.turn_off()
-    return 'OK'
 
 
 @app.route('/status')
@@ -73,12 +73,13 @@ def get_status():
 @app.route('/info')
 def get_info():
     logging.info(f'Hit info: {master.get_info()}')
-    return master.get_info()
+    return {'info': master.get_info(), 'counter': self.get_counter()}
 
 
 @app.route('/push_episode')
 def push_episode():
     data = request.get_json()
+    logging.info('Hit push_episode: {data}')
     master.push(data)
     return 'OK'
 
