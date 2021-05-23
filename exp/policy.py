7be32500a8092eeac3caa2778f4afbc994608b84
Sergio Chouhy <sergio.chouhy@gmail.com>
2021-05-23 22:57:45 +0000

    net

--- exp/policy.py
+++ exp/policy.py
@@ -14,17 +14,17 @@ class Network(nn.Module):
         self.action_size = num_actions
 
         super(Network, self).__init__()
-        self.conv1 = nn.Conv2d(1, 12, 3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(12, 12, 3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(12, 12, 3, stride=1)
-        self.conv4 = nn.Conv2d(12, 12, 3, stride=1)
+        self.conv1 = nn.Conv2d(12, 512, 3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(512, 512, 3, stride=1)
+        self.conv4 = nn.Conv2d(512, 512, 3, stride=1)
 
-        self.bn1 = nn.BatchNorm2d(12)
-        self.bn2 = nn.BatchNorm2d(12)
-        self.bn3 = nn.BatchNorm2d(12)
-        self.bn4 = nn.BatchNorm2d(12)
+        self.bn1 = nn.BatchNorm2d(512)
+        self.bn2 = nn.BatchNorm2d(512)
+        self.bn3 = nn.BatchNorm2d(512)
+        self.bn4 = nn.BatchNorm2d(512)
 
-        self.fc1 = nn.Linear(12*(self.board_x-4)*(self.board_y-4), 1024)
+        self.fc1 = nn.Linear(512*(self.board_x-4)*(self.board_y-4), 1024)
         self.fc_bn1 = nn.BatchNorm1d(1024)
 
         self.fc2 = nn.Linear(1024, 512)
@@ -41,12 +41,18 @@ class Network(nn.Module):
     def forward(self, s):
         if isinstance(s, np.ndarray):
             s = self._from_numpy(s)
-        s = s.view(-1, 1, self.board_x, self.board_y)                # batch_size x 1 x board_x x board_y
+        s = s.view(-1, 12, self.board_x, self.board_y)                # batch_size x 1 x board_x x board_y
+        print(s.shape)
         s = F.relu(self.bn1(self.conv1(s)))                          # batch_size x num_channels x board_x x board_y
+        print(s.shape)
         s = F.relu(self.bn2(self.conv2(s)))                          # batch_size x num_channels x board_x x board_y
+        print(s.shape)
         s = F.relu(self.bn3(self.conv3(s)))                          # batch_size x num_channels x (board_x-2) x (board_y-2)
+        print(s.shape)
         s = F.relu(self.bn4(self.conv4(s)))                          # batch_size x num_channels x (board_x-4) x (board_y-4)
-        s = s.view(-1, 12*(self.board_x-4)*(self.board_y-4))
+        print(s.shape)
+        s = s.view(-1, 512*(self.board_x-4)*(self.board_y-4))
+        print('wasaa')
 
         s = F.dropout(F.relu(self.fc_bn1(self.fc1(s))), p=0.3, training=self.training)  # batch_size x 1024
         s = F.dropout(F.relu(self.fc_bn2(self.fc2(s))), p=0.3, training=self.training)  # batch_size x 512
